94505220bb9c0f8b94238e29eff16b8c96d1346f
gauravjalap <luckyskchirag@gmail.com>
2024-10-02 17:52:06 +0530

    Refactor next.config.js to remove unoptimized image setting

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 const isProd = process.env.NODE_ENV === "production";
 const nextConfig = {
   images: {
-    unoptimized: true, // This is fine if you're not using the next/image optimization
+    // unoptimized: true, // This is fine if you're not using the next/image optimization
   },
   assetPrefix: isProd ? "/" : "",
   basePath: "", // Keep this if you're not deploying under a subdirectory
