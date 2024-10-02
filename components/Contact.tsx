2e2704a2c3eff5e2b45df7a8b4bbc5c89111f9be
gauravjalap <luckyskchirag@gmail.com>
2024-10-02 18:34:41 +0530

    Refactor Contact component to center align text and update button label

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -4,7 +4,7 @@ import SocialLink from "./SocialLink";
 const Contact = () => {
   return (
     <div className="wrapper">
-      <div className="max-w-5xl mx-auto flex flex-col gap-4 items-center justify-center">
+      <div className="max-w-5xl mx-auto flex flex-col gap-4 items-center justify-center text-center">
         <h2 className="text-2xl font-bold">Let us create awesome products!</h2>
         <p className="text-lg tracking-wide font-medium text-center">
           I am always open to discussing your project, improving your online
@@ -13,7 +13,7 @@ const Contact = () => {
         </p>
         <a href="mailto:work.gauravkumar01@gmail.com">
           <button className="w-52 h-14 bg-gradient-to-r from-blue-600 via-blue-700 to-indigo-950 text-xl font-bold text-white rounded-lg hover:from-indigo-950 hover:to-blue-600 duration-300">
-            Say Hello
+            Email Me
           </button>
         </a>
       </div>
